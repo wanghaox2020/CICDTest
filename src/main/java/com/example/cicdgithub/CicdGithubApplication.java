4ca68d96b58f41bfd2538b25062b147db2c2bfc0
wanghaox2020 <79470452+wanghaox2020@users.noreply.github.com>
2025-04-01 18:52:23 -0500

    first commit to github

---
+++ src/main/java/com/example/cicdgithub/CicdGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdGithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicdgithub/CicdGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
